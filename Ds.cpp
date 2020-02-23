31174ec1fb2b3034c2be5f7a90f4d8ec34be4098
东灯 <43312495+Lampese@users.noreply.github.com>
2020-02-24 04:03:27 +0800

    Add files via upload

---
+++ Ds.cpp
@@ -0,0 +1,11 @@
+#include "Ds.h";
+int main(){
+	CCL("abcdefg;sudnf{haha}dexer233;lampese666");
+	for(int i=0;i<rds;i++){
+		cout<<i+1<<":"<<endl;
+		cout<<rd[i].type<<endl;
+		cout<<rd[i].tg<<endl; 
+		cout<<rd[i].lang<<endl;
+	}
+	return 0;   
+}
